726361ade6cd682ad13951fcbd1a3a776826c360
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-04-08 00:48:28 +0300

    finished purchase request page

--- app/js/main.js
+++ app/js/main.js
@@ -1144,6 +1144,12 @@ if (selectPrimary.length >= 1) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var fullcalendar__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! fullcalendar */ "./node_modules/fullcalendar/index.js");
+/* harmony import */ var simplebar__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! simplebar */ "./node_modules/simplebar/dist/simplebar.esm.js");
+/* harmony import */ var _modules_disableScroll__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../modules/disableScroll */ "./src/js/modules/disableScroll.js");
+/* harmony import */ var _modules_enableScroll__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../modules/enableScroll */ "./src/js/modules/enableScroll.js");
+
+
+
 
 calendarPrimary();
 function calendarPrimary() {
@@ -1154,20 +1160,200 @@ function calendarPrimary() {
     locale: 'ru',
     dayMaxEvents: 1,
     moreLinkContent: obj => `+ еще ${obj.num}`,
-    events: [{
-      id: 1,
-      title: '1-комн. квартира,55м², 4/5эт. ',
-      start: '2023-04-03T09:45:00'
-    }, {
-      id: 1,
-      title: '1-комн. квартира,55м², 4/5эт. ',
-      start: '2023-04-03T09:45:00'
-    }]
-    // https://fullcalendar.io/api/demo-feeds/events.json?overload-day
+    fixedWeekCount: false,
+    eventClassNames: 'fc-event-container',
+    eventSources: [{
+      url: 'eventsCalendar.json'
+    }],
+    headerToolbar: {
+      left: 'title',
+      center: '',
+      right: 'customPrev,customNext'
+    },
+    eventContent: obj => {
+      return {
+        html: `
+            <span>${obj.timeText}</span>
+            <span>${obj.event._def.title}</span>
+            `
+      };
+    },
+    customButtons: {
+      customPrev: {
+        text: 'Предыдущий месяц',
+        icon: 'chevron-left',
+        click: function () {
+          calendaryPrimary.prev();
+          addedClassesCalendar();
+        }
+      },
+      customNext: {
+        text: 'Следующий месяц',
+        icon: 'chevron-right',
+        click: function () {
+          calendaryPrimary.next();
+          addedClassesCalendar();
+        }
+      }
+    }
   });
-
   calendaryPrimary.render();
+  getEvents();
+  async function getEvents() {
+    const response = await fetch('./eventsCalendar.json');
+    if (response.ok) {
+      const eventsArray = await response.json();
+      eventModal(eventsArray);
+      addedClassesCalendar();
+    }
+  }
+  function eventModal(eventsArray) {
+    calendarPrimaryEl.addEventListener('click', e => {
+      if (!(e.target.classList.contains('.fc-event') || e.target.closest('.fc-event'))) return false;
+      const event = e.target.closest('.fc-event');
+      const eventDate = event.closest('[data-date]').dataset.date;
+      const modalHTML = `
+            <div class="calendar-event" data-date="${eventDate}">
+            <div class="calendar-event__container">
+                <button class="btn-reset js-popup-close calendar-event__close" aria-label="Закрыть модальное окно">
+                    <svg>
+                        <use xlink:href="img/sprite.svg#x"></use>
+                    </svg>
+                    <span>Закрыть</span>
+                </button>
+                <div class="calendar-event__content">
+                    <h2 class="calendar-event__title">Объект просмотра</h2>
+                    <ul class="calendar-event__list list-reset calendar-event-simplebar">
+
+                    </ul>
+                </div>
+            </div>
+            </div>
+            `;
+      modal(modalHTML, '.calendar-event', 300);
+      eventsArray.forEach(el => {
+        if (el.date === eventDate) {
+          const itemHTML = `
+                <li class="calendar-event__item calendar-event-item">
+                    <div class="calendar-event-item__content">
+                        <div class="calendar-event-item__title">
+                            ${el.title}
+                        </div>
+                        <div class="calendar-event-item__location">
+                            <svg>
+                                <use xlink:href="img/sprite.svg#location"></use>
+                            </svg>
+                            ${el.location}
+                        </div>
+                        <span class="calendar-event-item__price">${el.price}</span>
+                        <div class="calendar-event-item__user user-info">
+                            <div class="user-info__avatar avatar online">
+                                <img loading="lazy" src="${el.user.avatar}" width="32" height="32" alt="${el.user.name}">
+                            </div>
+                            <span class="user-info__pos">
+                                ${el.user.pos}
+                            </span>
+                            <span class="user-info__name">
+                                ${el.user.name}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="calendar-event-item__time">
+                        <svg>
+                            <use xlink:href="img/sprite.svg#clock"></use>
+                        </svg>
+                        <div>
+                            <span>${el.time}</span>
+                            <span>${el.date}</span>
+                        </div>
+                    </div>
+                </li>
+                    `;
+          document.querySelector('.calendar-event__list').insertAdjacentHTML('beforeend', itemHTML);
+        }
+      });
+      const calendarEventSimplebar = document.querySelector('.calendar-event-simplebar');
+      new simplebar__WEBPACK_IMPORTED_MODULE_1__["default"](calendarEventSimplebar);
+    });
+  }
+  function addedClassesCalendar() {
+    setTimeout(() => {
+      document.querySelectorAll('.fc-event-container').forEach(el => {
+        if (el.closest('.fc-day')) el.closest('.fc-day').classList.add('fc-day-event');
+      });
+      document.querySelectorAll('.fc-day-event').forEach(el => {
+        const events = el.querySelectorAll('.fc-event');
+        for (let i = 0; i < events.length; i++) {
+          const circleHTML = `<span class="fc-event--circle">event</span>`;
+          events[0].insertAdjacentHTML('beforeend', circleHTML);
+        }
+      });
+    }, 100);
+  }
 }
+const modal = function (modalHTML, container) {
+  let speed = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 300;
+  if (document.querySelectorAll(container).length <= 0 && modalHTML) {
+    document.body.insertAdjacentHTML('beforeend', modalHTML);
+    const modalEl = document.querySelector(container);
+    const modalContainerEl = modalEl.querySelector(`${container}__container`);
+    const modalCloseEl = modalEl.querySelector(`${container}__close`);
+    const settingsModal = {
+      modal: modalEl,
+      container: modalContainerEl,
+      isOpen: false,
+      speed: speed,
+      animation: 'fade'
+    };
+    setTimeout(() => {
+      modalOpen(settingsModal);
+    }, 1);
+    modalCloseEl.addEventListener('click', () => {
+      modalClose(settingsModal);
+    });
+    modalEl.addEventListener('click', e => {
+      if (e.target.classList.contains(container.replace(/^\./, ""))) {
+        modalClose(settingsModal);
+      }
+    });
+    window.addEventListener('keydown', e => {
+      if (e.keyCode == 27) {
+        modalClose(settingsModal);
+      }
+    });
+  }
+  function modalClose(settingsModal) {
+    if (settingsModal.isOpen) {
+      settingsModal.container.classList.remove('animate-open');
+      settingsModal.container.classList.remove(settingsModal.animation);
+      settingsModal.modal.classList.remove('is-open');
+      settingsModal.container.classList.remove('open');
+      (0,_modules_enableScroll__WEBPACK_IMPORTED_MODULE_3__["default"])();
+      document.body.style.scrollBehavior = 'auto';
+      document.documentElement.style.scrollBehavior = 'auto';
+      setTimeout(() => {
+        settingsModal.modal.remove();
+      }, settingsModal.speed);
+      settingsModal.isOpen = false;
+    }
+  }
+  function modalOpen(settingsModal) {
+    if (!settingsModal.isOpen) {
+      settingsModal.container.scrollTo(0, 0);
+      settingsModal.modal.style.setProperty('--transition-time', `${settingsModal.speed / 1000}s`);
+      settingsModal.modal.classList.add('is-open');
+      document.body.style.scrollBehavior = 'auto';
+      document.documentElement.style.scrollBehavior = 'auto';
+      (0,_modules_disableScroll__WEBPACK_IMPORTED_MODULE_2__["default"])();
+      settingsModal.container.classList.add('open');
+      settingsModal.container.classList.add(settingsModal.animation);
+      setTimeout(() => {
+        settingsModal.container.classList.add('animate-open');
+      }, settingsModal.speed);
+      settingsModal.isOpen = true;
+    }
+  }
+};
 
 /***/ }),
 
--- src/js/components/calendar.js
+++ src/js/components/calendar.js
@@ -1,16 +1,12 @@
 import {
     Calendar
 } from 'fullcalendar';
+import SimpleBar from 'simplebar';
+import disableScroll from '../modules/disableScroll';
+import enableScroll from '../modules/enableScroll';
 
 
 calendarPrimary();
-
-
-
-
-
-
-
 
 
 function calendarPrimary() {
@@ -21,19 +17,208 @@ function calendarPrimary() {
         locale: 'ru',
         dayMaxEvents: 1,
         moreLinkContent: (obj) => `+ еще ${obj.num}`,
-        events: [{
-                id: 1,
-                title: '1-комн. квартира,55м², 4/5эт. ',
-                start: '2023-04-03T09:45:00'
+        fixedWeekCount: false,
+        eventClassNames: 'fc-event-container',
+        eventSources: [{
+            url: 'eventsCalendar.json',
+        }],
+        headerToolbar: {
+            left: 'title',
+            center: '',
+            right: 'customPrev,customNext',
+        },
+        eventContent: (obj) => {
+            return {
+                html: `
+            <span>${obj.timeText}</span>
+            <span>${obj.event._def.title}</span>
+            `
+            }
+        },
+        customButtons: {
+            customPrev: {
+                text: 'Предыдущий месяц',
+                icon: 'chevron-left',
+                click: function () {
+                    calendaryPrimary.prev();
+                    addedClassesCalendar();
+                }
             },
-            {
-                id: 1,
-                title: '1-комн. квартира,55м², 4/5эт. ',
-                start: '2023-04-03T09:45:00'
-            },
-        ],
-        // https://fullcalendar.io/api/demo-feeds/events.json?overload-day
+            customNext: {
+                text: 'Следующий месяц',
+                icon: 'chevron-right',
+                click: function () {
+                    calendaryPrimary.next();
+                    addedClassesCalendar();
+                }
+            }
+        }
     })
 
     calendaryPrimary.render();
+
+    getEvents();
+    async function getEvents() {
+        const response = await fetch('./eventsCalendar.json');
+        if (response.ok) {
+            const eventsArray = await response.json();
+            eventModal(eventsArray);
+            addedClassesCalendar();
+        }
+    }
+
+    function eventModal(eventsArray) {
+        calendarPrimaryEl.addEventListener('click', (e) => {
+            if (!(e.target.classList.contains('.fc-event') || e.target.closest('.fc-event'))) return false;
+            const event = e.target.closest('.fc-event');
+            const eventDate = event.closest('[data-date]').dataset.date;
+
+            const modalHTML = `
+            <div class="calendar-event" data-date="${eventDate}">
+            <div class="calendar-event__container">
+                <button class="btn-reset js-popup-close calendar-event__close" aria-label="Закрыть модальное окно">
+                    <svg>
+                        <use xlink:href="img/sprite.svg#x"></use>
+                    </svg>
+                    <span>Закрыть</span>
+                </button>
+                <div class="calendar-event__content">
+                    <h2 class="calendar-event__title">Объект просмотра</h2>
+                    <ul class="calendar-event__list list-reset calendar-event-simplebar">
+
+                    </ul>
+                </div>
+            </div>
+            </div>
+            `;
+            modal(modalHTML, '.calendar-event', 300);
+            eventsArray.forEach(el => {
+                if (el.date === eventDate) {
+                    const itemHTML = `
+                <li class="calendar-event__item calendar-event-item">
+                    <div class="calendar-event-item__content">
+                        <div class="calendar-event-item__title">
+                            ${el.title}
+                        </div>
+                        <div class="calendar-event-item__location">
+                            <svg>
+                                <use xlink:href="img/sprite.svg#location"></use>
+                            </svg>
+                            ${el.location}
+                        </div>
+                        <span class="calendar-event-item__price">${el.price}</span>
+                        <div class="calendar-event-item__user user-info">
+                            <div class="user-info__avatar avatar online">
+                                <img loading="lazy" src="${el.user.avatar}" width="32" height="32" alt="${el.user.name}">
+                            </div>
+                            <span class="user-info__pos">
+                                ${el.user.pos}
+                            </span>
+                            <span class="user-info__name">
+                                ${el.user.name}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="calendar-event-item__time">
+                        <svg>
+                            <use xlink:href="img/sprite.svg#clock"></use>
+                        </svg>
+                        <div>
+                            <span>${el.time}</span>
+                            <span>${el.date}</span>
+                        </div>
+                    </div>
+                </li>
+                    `;
+                    document.querySelector('.calendar-event__list').insertAdjacentHTML('beforeend', itemHTML);
+                }
+            })
+            const calendarEventSimplebar = document.querySelector('.calendar-event-simplebar');
+            new SimpleBar(calendarEventSimplebar);
+        });
+    }
+
+    function addedClassesCalendar() {
+        setTimeout(() => {
+            document.querySelectorAll('.fc-event-container').forEach(el => {
+                if (el.closest('.fc-day')) el.closest('.fc-day').classList.add('fc-day-event');
+            });
+            document.querySelectorAll('.fc-day-event').forEach(el => {
+                const events = el.querySelectorAll('.fc-event');
+                for (let i = 0; i < events.length; i++) {
+                    const circleHTML = `<span class="fc-event--circle">event</span>`;
+                    events[0].insertAdjacentHTML('beforeend', circleHTML);
+                }
+            });
+        }, 100);
+    }
+
+}
+
+
+
+
+const modal = (modalHTML, container, speed = 300) => {
+    if (document.querySelectorAll(container).length <= 0 && modalHTML) {
+        document.body.insertAdjacentHTML('beforeend', modalHTML);
+        const modalEl = document.querySelector(container);
+        const modalContainerEl = modalEl.querySelector(`${container}__container`);
+        const modalCloseEl = modalEl.querySelector(`${container}__close`);
+        const settingsModal = {
+            modal: modalEl,
+            container: modalContainerEl,
+            isOpen: false,
+            speed: speed,
+            animation: 'fade'
+        };
+        setTimeout(() => {
+            modalOpen(settingsModal);
+        }, 1);
+        modalCloseEl.addEventListener('click', () => {
+            modalClose(settingsModal);
+        });
+        modalEl.addEventListener('click', (e) => {
+            if (e.target.classList.contains(container.replace(/^\./, ""))) {
+                modalClose(settingsModal);
+            }
+        })
+        window.addEventListener('keydown', (e) => {
+            if (e.keyCode == 27) {
+                modalClose(settingsModal);
+            }
+        })
+    }
+
+    function modalClose(settingsModal) {
+        if (settingsModal.isOpen) {
+            settingsModal.container.classList.remove('animate-open');
+            settingsModal.container.classList.remove(settingsModal.animation);
+            settingsModal.modal.classList.remove('is-open');
+            settingsModal.container.classList.remove('open');
+            enableScroll();
+            document.body.style.scrollBehavior = 'auto';
+            document.documentElement.style.scrollBehavior = 'auto';
+            setTimeout(() => {
+                settingsModal.modal.remove();
+            }, settingsModal.speed);
+            settingsModal.isOpen = false;
+        }
+    }
+
+    function modalOpen(settingsModal) {
+        if (!settingsModal.isOpen) {
+            settingsModal.container.scrollTo(0, 0);
+            settingsModal.modal.style.setProperty('--transition-time', `${settingsModal.speed / 1000}s`);
+            settingsModal.modal.classList.add('is-open');
+            document.body.style.scrollBehavior = 'auto';
+            document.documentElement.style.scrollBehavior = 'auto';
+            disableScroll();
+            settingsModal.container.classList.add('open');
+            settingsModal.container.classList.add(settingsModal.animation);
+            setTimeout(() => {
+                settingsModal.container.classList.add('animate-open');
+            }, settingsModal.speed);
+            settingsModal.isOpen = true;
+        }
+    }
 }
