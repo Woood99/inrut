4df7b0c13598d7ab7a8e2dd3e627797d49167407
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-06-25 14:58:54 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -9148,21 +9148,35 @@ const dropdown = (containerSelector, targetSelector) => {
   container.forEach(el => {
     const target = el.querySelector(targetSelector);
     const chatItem = el.closest('.chat__item');
-    target.addEventListener('click', e => {
-      e.preventDefault();
-      container.forEach(el => {
-        if (e.target.closest(containerSelector) !== el) {
-          el.classList.remove('_active');
-          if (el.closest('.chat__item')) {
-            el.closest('.chat__item').classList.remove('_dropdown-active');
+    if (!el.classList.contains('_hover')) {
+      target.addEventListener('click', e => {
+        e.preventDefault();
+        container.forEach(el => {
+          if (e.target.closest(containerSelector) !== el) {
+            el.classList.remove('_active');
+            if (el.closest('.chat__item')) {
+              el.closest('.chat__item').classList.remove('_dropdown-active');
+            }
           }
+        });
+        el.classList.toggle('_active');
+        if (chatItem) {
+          chatItem.classList.toggle('_dropdown-active');
         }
       });
-      el.classList.toggle('_active');
-      if (chatItem) {
-        chatItem.classList.toggle('_dropdown-active');
-      }
-    });
+    } else {
+      target.addEventListener('click', e => {
+        if (window.innerWidth <= 1144) {
+          e.preventDefault();
+          container.forEach(el => {
+            if (e.target.closest(containerSelector) !== el) {
+              el.classList.remove('_active');
+            }
+          });
+          el.classList.toggle('_active');
+        }
+      });
+    }
     document.addEventListener('click', e => {
       if (!e.target.closest(containerSelector)) {
         if (el.classList.contains('_active')) el.classList.remove('_active');
